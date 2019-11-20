d256c9d889e2304660c594531eaf3f04df0c5a98
Bretay <albretney@gmail.com>
2019-11-20 11:23:10 -0800

    toggleTurn implemented with tests

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -4,14 +4,14 @@
 
 #include <gtest/gtest.h>
 #include "TicTacToeBoard.h"
- 
+
 class TicTacToeBoardTest : public ::testing::Test
 {
 	protected:
 		TicTacToeBoardTest(){} //constructor runs before each test
 		virtual ~TicTacToeBoardTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 /* EXAMPLE TEST FORMAT
@@ -20,3 +20,12 @@ TEST(TicTacToeBoardTest, unitTestName)
 	ASSERT_TRUE(true);
 }
 */
+
+TEST(TicTacToeBoardTest, toggleX) {
+  ASSERT_EQ(toggleTurn(), Piece::O);
+}
+
+TEST(TicTacToeBoardTest, toggleY) {
+  toggleTurn();
+  ASSERT_EQ(toggleTurn(), Piece::X);
+}
